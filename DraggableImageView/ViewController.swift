c7fa8aa05c10fa43da364346f2d2a9d2ad659857
Matt Blessed <matthew.blessed@hope.edu>
2016-01-23 16:21:10 -0500

    Initial Commit

---
+++ DraggableImageView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DraggableImageView
+//
+//  Created by Matt B on 1/23/16.
+//  Copyright © 2016 Matt Blessed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
